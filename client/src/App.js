ae649fcaf584f94258ba08a6126683587d028b0a
Ryan Montgomery <coap.ryanmontgomery@gmail.com>
2024-10-18 16:46:18 -0600

    added revised App w set token prop

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,7 @@ import Register from './components/Register';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import PatentCards from './components/PatentCard';
-// import DeepPatentabilityAnalysis from './components/PatentabilityAnalysis';
 import PriorArtComponent from './components/PriorArtComponent';
-
 import Logout from './components/Logout';
 import ErrorBoundary from './components/ErrorBoundary';
 import CreatePatentForm from './components/CreatePatentForm';
@@ -17,35 +15,31 @@ import UpdatePatentForm from './components/UpdatePatentForm';
 import UserAccount from './components/UserAccount';
 import { UserProvider } from './context/UserContext';
 
-
 function App() {
-
-  
-
+  const [token, setToken] = useState(null);
 
   return (
     <UserProvider>
-    <Router>
-      <div className="App">
-        <Header />
-        <ErrorBoundary>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register  />} />
-            <Route path="/login" element={<Login  />} />
-            <Route path="/create-patent" element={<CreatePatentForm/>} />
-            <Route path="/patents/:patentId/update" element={<UpdatePatentForm />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/patents" element={<PatentCards />} />
-            {/* <Route path="/patents/:id/analysis" element={<DeepPatentabilityAnalysis />} /> */}
-            <Route path="/patents/:id/prior-art" element={<PriorArtComponent />} />
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/user-account" element={<UserAccount />} />
-          </Routes>
-        </ErrorBoundary>
-        <Footer />
-      </div>
-    </Router>
+      <Router>
+        <div className="App">
+          <Header />
+          <ErrorBoundary>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/register" element={<Register setToken={setToken} />} />
+              <Route path="/login" element={<Login setToken={setToken} />} />
+              <Route path="/create-patent" element={<CreatePatentForm />} />
+              <Route path="/patents/:patentId/update" element={<UpdatePatentForm />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/patents" element={<PatentCards />} />
+              <Route path="/patents/:id/prior-art" element={<PriorArtComponent />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/user-account" element={<UserAccount />} />
+            </Routes>
+          </ErrorBoundary>
+          <Footer />
+        </div>
+      </Router>
     </UserProvider>
   );
 }
